af65d8717d06222796c39273b29625283bb322da
Akhilesh Kamble <akhilesh.kamble@mavenir.com>
2022-05-15 22:02:18 +0530

    TypeWriter effect for homepage

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Home from './Home';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
 import NotFound from './NotFound';
+import NewHome from './NewHome';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
+
+
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar/>
         <div className="content">
           <Routes>
-            <Route exact path="/" element={<Home />} />
+            <Route exact path="/" element={<NewHome />} />
             <Route path="/create" element={<Create />} />             
             <Route path="/blogs/:id" element={<BlogDetails />} />           
             <Route path="*" element={<NotFound />} />          
